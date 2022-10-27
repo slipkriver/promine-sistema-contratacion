9a2f10ba608ccf7c64fa7cafc84c0f92e3abdf4d
byros21 <byros3@yahoo.com>
2022-10-27 05:50:38 -0500

    cambio componente item-aspirante *svg banner-item +shadow

--- src/app/componentes/item-aspirante/item-aspirante.component.ts
+++ src/app/componentes/item-aspirante/item-aspirante.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { addIcons } from 'ionicons';
+
 @Component({
   selector: 'app-item-aspirante',
   templateUrl: './item-aspirante.component.html',
@@ -12,7 +14,15 @@ export class ItemAspiranteComponent implements OnInit {
   @Input("index") index;
 
 
-  constructor( ) { }
+  constructor( ) { 
+
+    addIcons({
+      'banner-item': 'assets/icon/banner-item.svg',
+      'banner-item-outline': 'assets/icon/banner-item-outline.svg',
+      //'flag-de': 'assets/flags/de.svg'
+    });
+
+  }
 
   ngOnInit() {}
 
