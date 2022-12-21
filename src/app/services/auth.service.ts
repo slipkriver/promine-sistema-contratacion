8db3e0527b633e8e7c0a0b2c32a480a5f315b81c
byros21 <byros21@gmail.com>
2022-12-21 04:40:51 -0500

    fix login page + auth.service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, authState, signOut, authInstance$ } from '@angular/fire/auth';
-import { DataLocalService } from './data-local.service';
+import { DataService } from './data.service';
+
 
 import JSEncrypt from 'jsencrypt';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
@@ -15,37 +17,40 @@ export class AuthService {
   privCode = `MIICXQIBAAKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQABAoGAfY9LpnuWK5Bs50UVep5c93SJdUi82u7yMx4iHFMc/Z2hfenfYEzu+57fI4fvxTQ//5DbzRR/XKb8ulNv6+CHyPF31xk7YOBfkGI8qjLoq06V+FyBfDSwL8KbLyeHm7KUZnLNQbk8yGLzB3iYKkRHlmUanQGaNMIJziWOkN+N9dECQQD0ONYRNZeuM8zd8XJTSdcIX4a3gy3GGCJxOzv16XHxD03GW6UNLmfPwenKu+cdrQeaqEixrCejXdAFz/7+BSMpAkEA8EaSOeP5Xr3ZrbiKzi6TGMwHMvC7HdJxaBJbVRfApFrE0/mPwmP5rN7QwjrMY+0+AbXcm8mRQyQ1+IGEembsdwJBAN6az8Rv7QnD/YBvi52POIlRSSIMV7SwWvSK4WSMnGb1ZBbhgdg57DXaspcwHsFV7hByQ5BvMtIduHcT14ECfcECQATeaTgjFnqE/lQ22Rk0eGaYO80cc643BXVGafNfd9fcvwBMnk0iGX0XRsOozVt5AzilpsLBYuApa66NcVHJpCECQQDTjI2AQhFc1yRnCU/YgDnSpJVm1nASoRUnU8Jfm3Ozuku7JUXcVpt08DFSceCEX9unCuMcT72rAQlLpdZir876`;
   publiCode = `MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQAB`;
 
-  userLogin = {
+  userLogin:User;
+
+  userNew: User  = {
+    uid: '',
     cedula: '0000000000',
     email: '',
-    password: null,
-    nombre: 'Usuario *TTHH',
+    password: '',
+    displayName: 'Usuario *TTHH',
+    emailVerified:true,
     role: 'tthh',
-    fechalogin: new Date().toLocaleString()
+    fechalogin: new Date(),
   }
 
-  userLocal = {};
+  userLocal:User;
+  getuserlogin$ = new EventEmitter<any>();
+
 
   constructor(
     private auth: Auth,
-    private datalocal: DataLocalService
+    private dataService: DataService
   ) {
 
-    datalocal.getUserConfig().then(conf => {
+    this.userLogin = {... this.userNew}
+    dataService.mostrarLoading$.emit(true)
+    dataService.dataLocal.getUserConfig().then(conf => {
       this.userConfig = conf || {};
       this.userLocal = this.userConfig['user'];
       //console.log(conf['user']);
       //this.encryptPassword('123456')
+      this.getUserLoging();
+
     });
-
-    auth;
-    this.getUserLoging()
-
-    setTimeout(() => {
-      //console.log(this.userConfig['user'])
-      //this.logout()
-      //this.loginToken(this.userConfig['user'].stsTokenManager.accessToken)
-    }, 8000);
+    //auth;
+    //this.getUserLoging()
 
 
   }
@@ -66,46 +71,48 @@ export class AuthService {
 
   //logout
   logout() {
-    this.datalocal.setConfig("user",{})
+    this.dataService.dataLocal.setConfig("user",{})
+    this.userLocal = null; 
+    //this.userLogin = {...this.userNew}; 
+    //console.log("Log OFF ")
     return signOut(this.auth);
   }
 
   async getUserLoging() {
 
-    this.auth.onAuthStateChanged((user: any) => {
-      if (user) {
-        //console.log("**getUser ",user)
+    // console.log("**getUser ",this.userLocal, "..NO USER", this.userLogin)
+
+    return this.auth.onAuthStateChanged((user: any) => {
+      if (user?.email) {
         //user[password]
-        this.userLogin = {... user};
+        this.userLogin = { ...user };
       } else {
-        this.userLogin = {... user};
-        this.userLogin.fechalogin = new Date().toISOString();
-        this.userLogin.password = this.uncryptPassword(this.userLocal['password'])
-        //console.log("**getUser ", "..NO USER", user, this.userLogin)
+        // console.log(this.userLocal)
+        this.userLogin = { ...user };
+
+        this.userLogin.fechalogin = new Date();
+        this.userLogin.password = this.uncryptPassword(this.userLocal['password']).toString()
         //this.login(user)
       }
+      // console.log(user);
+      this.getuserlogin$.emit(user)
       //x = user
-      //console.log(user)
     });
 
   }
 
-  setUserLoging() {
-    const usuario = {
-      cedula: '0000000000',
-      email: this.userLogin.email,
-      password: this.userLogin.password,
-      nombre: 'Usuario *TTHH',
-      role: 'tthh',
-      fechalogin: new Date().toLocaleString()
-    }
+  setUserLoging(email, password) {
+    const usuario = {... this.userNew} ;
+    usuario.uid= this.userLogin.uid;
+    usuario.email= email;
+    usuario.password= this.encryptPassword(password).toString();
+    usuario.role= 'tthh';
+    usuario.fechalogin = new Date(Date.now());
     //this.userConfig['user'] = user;
-    console.log(usuario)
-
-    this.datalocal.setConfig("user", usuario)
-    this.datalocal.setConfig("role", usuario.role)
-    setTimeout(() => {
-    }, 1000);
+    // console.log(usuario, this.userLocal, this.userLogin)
+    this.userLocal = usuario;
+    this.dataService.dataLocal.setConfig("user", usuario)
+    this.dataService.dataLocal.setConfig("role", usuario.role)
 
   }
 
@@ -134,5 +141,11 @@ export class AuthService {
 
     return uncrypted;
   }
+
+  mostrarLoading( show ){
+    // console.log("Mostrar **Loading:", show)
+    this.dataService.mostrarLoading$.emit(show)
+  }
+
 
 }
