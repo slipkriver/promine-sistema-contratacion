c69bb25392ddb40a016af05ec25fa523722c8422
Elias Rivera <slipkriver@gmail.com>
2023-03-23 16:53:57 -0500

    Creando la page aspirante-uafe para el formulario UAFE

--- src/app/pages/aspirante-uafe/aspirante-uafe.module.ts
+++ src/app/pages/aspirante-uafe/aspirante-uafe.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
@@ -7,14 +7,32 @@ import { IonicModule } from '@ionic/angular';
 import { AspiranteUafePageRoutingModule } from './aspirante-uafe-routing.module';
 
 import { AspiranteUafePage } from './aspirante-uafe.page';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule } from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AspiranteUafePageRoutingModule
+    AspiranteUafePageRoutingModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
-  declarations: [AspiranteUafePage]
+  declarations: [AspiranteUafePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'es-EC'},
+    DatePipe
+  ]
 })
 export class AspiranteUafePageModule {}
