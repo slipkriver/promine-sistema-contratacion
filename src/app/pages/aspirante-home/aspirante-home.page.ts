666f4615bdaebbc18368194bb530ac8040883099
byros21 <byros21@gmail.com>
2023-05-20 23:36:15 -0500

    limpieza y actualizacion de paquetes

--- src/app/pages/aspirante-home/aspirante-home.page.ts
+++ src/app/pages/aspirante-home/aspirante-home.page.ts
@@ -5,11 +5,52 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './aspirante-home.page.html',
   styleUrls: ['./aspirante-home.page.scss'],
 })
-export class AspiranteHomePage implements OnInit {
+export class AspiranteHomePage {
 
-  constructor() { }
+  view: [number, number] = [700, 400];
+  
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  
+  colorScheme = JSON.stringify( {
+    domain: ["#5AA454", "#A10A28", "#C7B42C", "#AAAAAA"]
+  });
+  
+  single = [
+    {
+      "name": "Germany",
+      "value": 8940000
+    },
+    {
+      "name": "USA",
+      "value": 5000000
+    },
+    {
+      "name": "France",
+      "value": 7200000
+    },
+      {
+      "name": "UK",
+      "value": 6200000
+    }
+  ];
 
-  ngOnInit() {
+  constructor() {
+    //Object.assign(this, { single });
   }
 
+  onSelect(data:any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data:any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data:any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 }
